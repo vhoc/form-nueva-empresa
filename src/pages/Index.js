a9c5c14f68eabcc76002a330d6c93eb039c02faa
vhoc <vhocar@gmail.com>
2022-01-12 16:52:37 -0700

    Adjustments to the login routine

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 import axios from 'axios'
 
 import { validateStoredUser, logOut } from '../Helpers'
 import MenuPrincipal from "../components/MenuPrincipal/MenuPrincipal"
 import EmpresasTable from "../components/EmpresasTable/EmpresasTable"
+import { NavItem } from 'react-bootstrap'
 
 const Index = () => {
 
     const redirect = useNavigate()
-
+/*
     const validateToken = async ( token ) => {
 
         try {
@@ -27,7 +28,7 @@ const Index = () => {
         }
 
     }
-    /*
+    
     const validateStoredUser = ( token, userId, userName, userEmail ) => {
 
         // check if all props are supplied from localStorage
@@ -40,26 +41,30 @@ const Index = () => {
         // Prop(s) missing.
         return false
 
-    }*/
+    }
 /*
     useEffect( () => {
         //validateStoredUser( 'a token', 'an user id', 'an userName', 'an userEmail' )
         validateStoredUser( 'a token', 'an user id', 'an userName', 'an userEmail' ) ? 
     }, [] )*/
 
-
-    return (
-
-        <>
-            { validateStoredUser('a token', 'an user id', 'an userName', 'and userEmail') === true &&
+    if ( validateStoredUser('a token', 'an user id', 'an userName', 'and userEmail') === true )
+    {
+        return (
             <div className="container-fluid p-0">
                 <MenuPrincipal />
                 <EmpresasTable />
             </div>
-            }
-        </>
 
+        )
+    }
+    
+    return (
+        <Navigate to={"/login"} />
     )
+
+
+    
 }
 
 export default Index
