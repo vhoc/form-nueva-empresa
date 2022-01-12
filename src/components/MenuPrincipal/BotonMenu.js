bb148590bfcb558554ac585e93f659a936539955
vhoc <vhocar@gmail.com>
2022-01-12 15:11:28 -0700

    Adjustments to the login routine

--- src/components/MenuPrincipal/BotonMenu.js
+++ src/components/MenuPrincipal/BotonMenu.js
@@ -1,14 +1,33 @@
 import { Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './BotonMenu.css'
+import axios from "axios"
 
-const BotonMenu = ( {titulo, variant, icon, onKeyUp} ) => {
+const BotonMenu = ( {titulo, variant, icon, logoutBtn = false} ) => {
+
+    const logOut = () => {
+        console.log('intentando logout')
+        axios.get('https://venka.app/api/logout', {
+            headers: {
+                'Authorization': localStorage.getItem('token'),
+            }
+        }).then( response => {
+            console.log(response)
+        } )
+    }
 
     return (
         <>
         <Button
             variant={variant}
-            onKeyUp={onKeyUp}
+            onKeyUp={ event => {
+                if ( logoutBtn === true )
+                {
+                    logOut()
+                } else {
+                    event.preventDefault()
+                }
+            } }
             className="boton-menu d-flex flex-column align-items-center justify-content-center"
         >
             <FontAwesomeIcon icon={ icon } />
--- src/components/MenuPrincipal/MenuPrincipal.js
+++ src/components/MenuPrincipal/MenuPrincipal.js
@@ -1,7 +1,6 @@
 import BarraTitulo from "../BarraTitulo/BarraTitulo"
 import BotonMenu from "./BotonMenu"
 import { Link } from 'react-router-dom'
-import axios from "axios"
 
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faQuestion } from '@fortawesome/free-solid-svg-icons'
@@ -9,17 +8,6 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 const MenuPrincipal = () => {
-
-    const logOut = () => {
-        console.log('intentando logout')
-        axios.get('https://venka.app/api/logout', {
-            headers: {
-                'Authorization': localStorage.getItem('token'),
-            }
-        }).then( response => {
-            console.log(response)
-        } )
-    }
 
     return (
 
@@ -31,7 +19,7 @@ const MenuPrincipal = () => {
                 <Link to="/nueva-empresa"><BotonMenu titulo="Nueva Empresa" variant="info" icon={ faPlus }/></Link>
                 <BotonMenu titulo="Soporte" variant="warning" icon={ faQuestion }/>
                 <BotonMenu titulo="Suscripciones" variant="success" icon={ faShoppingCart }/>
-                <BotonMenu titulo="Salir" variant="primary" icon={ faSignOutAlt } onKeyUp={logOut} />
+                <BotonMenu titulo="Salir" variant="primary" icon={ faSignOutAlt } logoutBtn={true} />
             </div>
         
         </>
