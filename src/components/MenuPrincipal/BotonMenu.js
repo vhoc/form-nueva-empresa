704309f5574369cc8b7fbfc2ef079db6fb71bc16
vhoc <vhocar@gmail.com>
2022-01-13 15:04:17 -0700

    Adjustments to the login routine

--- src/components/MenuPrincipal/BotonMenu.js
+++ src/components/MenuPrincipal/BotonMenu.js
@@ -30,7 +30,7 @@ const BotonMenu = ( {titulo, variant, icon, logoutBtn} ) => {
             onClick={ event => {
                 if ( logoutBtn === true )
                 {
-                    console.log( await logOut() )
+                    console.log( logOut() )
                 } else {
                     event.preventDefault()
                 }
