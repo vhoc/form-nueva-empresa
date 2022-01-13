c93332dc355522cc72812385282c3bd0cf4e7231
vhoc <vhocar@gmail.com>
2022-01-13 11:56:31 -0700

    Adjustments to the login routine

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ function App() {
           </RequireAuth>
         }/>
         
-        <Route path="login" element={ <LoginForm redirectRoute="/" /> } />
+        <Route path="login" element={ 
+          <NoAuth redirectTo="/">
+            <LoginForm redirectRoute="/" />
+          </NoAuth>
+         } />
         <Route path="nueva-empresa" element={ <NuevaEmpresa/> } />
       </Routes>
     </div>
@@ -33,6 +37,12 @@ const RequireAuth = ({children, redirectTo}) => {
   let isAuthenticated = validateAuth()
   console.log(isAuthenticated)
   return isAuthenticated ? children : <Navigate to={redirectTo} />
+}
+
+const NoAuth = ({children, redirectTo}) => {
+  let isAnonymous = !validateAuth()
+  console.log(isAnonymous)
+  return isAnonymous ? children : <Navigate to={redirectTo}/>
 }
 
 export default App;
