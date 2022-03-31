100145c382861b8c2f07a9276677438650982a03
vhoc <vhocar@gmail.com>
2022-03-31 09:03:08 -0700

    Relocated header items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,10 @@ const Header = ({ title }) => {
 
     return (
         <div className="jumbotron jumbotron-fluid d-flex align-items-center px-0">
-            <div className="container-fluid d-flex align-items-center text-white text-left py-2 gap-3">
-                <h1 className="display-4 text-left text-uppercase">{title}</h1>
+            <div className="container-fluid d-flex align-items-center justify-content-between text-white text-left py-2 gap-3">
                 <p className="lead text-right">Mi Cuenta</p>
+                <h1 className="display-4 text-left text-uppercase">{title}</h1>
+                <span></span>
             </div>
         </div>
     )
